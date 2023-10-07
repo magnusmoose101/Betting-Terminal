1d2c1484cf1bbbbba027fd6aa49e4f764c651b60
magnusmoose101 <97981407+magnusmoose101@users.noreply.github.com>
2023-10-07 11:36:21 +0000

    Fully functioning login system.

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import time
+import json
+import os
+import hashlib
 import colorama
 from colorama import Fore
 
@@ -14,5 +17,56 @@ Print(Fore.GREEN + "Do you have an account?")
 Print(Fore.GREEN + "  1) Yes")
 Print(Fore.GREEN + "  2) No")
 
+accountOrNot = str(input(Fore.YELLOW + "~/ "))
 
+while True:
+  if accountOrNot == "1" or accountOrNot.lower() == "yes":
+    Print(Fore.GREEN + "Username:")
+    username = str(input(Fore.YELLOW + "~/ "))
+    Print(Fore.GREEN + "Password:")
+    password = str(input(Fore.YELLOW + "~/ "))
   
+    if os.path.exists(username + ".json"):
+      with open(username + ".json", "r") as openJsonReadable:
+        user = json.load(openJsonReadable)
+    
+      if user["password"] == hashlib.sha256(password.encode()).hexdigest():
+        Print(Fore.GREEN + "You're succesfully logged in.")
+        loggedIn = True
+        break
+      else:
+        Print(Fore.GREEN + "Your username or password is incorrect.")
+        loggedIn = False
+        break
+    else:
+      Print(Fore.GREEN + "Your username or password is incorrect.")
+      loggedIn = False
+      break
+  elif accountOrNot == "2" or accountOrNot.lower() == "no":
+    Print(Fore.GREEN + "Username:")
+    username = str(input(Fore.YELLOW + "~/ "))
+    Print(Fore.GREEN + "Password:")
+    password = str(input(Fore.YELLOW + "~/ "))
+    Print(Fore.GREEN + "First name:")
+    firstName = str(input(Fore.YELLOW + "~/ "))
+    Print(Fore.GREEN + "Last name:")
+    lastName = str(input(Fore.YELLOW + "~/ "))
+  
+    if os.path.exists(username + ".json") == False:
+      user = {"username": username, "password": hashlib.sha256(password.encode()).hexdigest(), "firstName": firstName, "lastName": lastName}
+      
+      with open(username + ".json", "w") as openJsonWritable:
+        json.dump(user, openJsonWritable, indent=2)
+        
+      loggedIn = True
+      break
+    else:
+      Print(Fore.GREEN + "This account already exists.")
+      loggedIn = False
+      break
+  else:
+    Print(Fore.GREEN + "That isn't an option.")
+    Print(Fore.GREEN + "Do you have an account?")
+    Print(Fore.GREEN + "  1) Yes")
+    Print(Fore.GREEN + "  2) No")
+    accountOrNot = str(input(Fore.YELLOW + "~/ "))
