bc6d5c1c2a571d8366f138a30212317e2492c5c4
magnusmoose101 <97981407+magnusmoose101@users.noreply.github.com>
2023-10-07 11:04:52 +0000

    Starting prints

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import colorama
 from colorama import Fore
 
 def Print(string):
+  print(Fore.GREEN + "~/ ", end="")
   for i in string:
     time.sleep(0.05)
     print(i, end="", flush=True)
+  print("")
     
-Print(Fore.GREEN + "Hi my name is Magnus")
+Print(Fore.GREEN + "Welcome to the hacking terminal.")
+Print(Fore.GREEN + "Do you have an account?")
+Print(Fore.GREEN + "  1) Yes")
+Print(Fore.GREEN + "  2) No")
+
+
+  
